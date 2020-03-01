9ed27517d6c50219100a173adaaf3e4cae2b40cf
Renato Moran <rveiga1992@gmail.com>
2020-03-01 01:27:46 -0300

    Terceiro commit

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 //Select element function
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+/*new for me*/
+menuToggler.addEventListener('click', function () {
+    body.classList.toggle('open');
+});
